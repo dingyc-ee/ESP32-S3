156999d8c95aef7546db5f3cf69609680d24507a
ding <1102004097@qq.com>
2022-12-04 12:09:38 +0800

    创建工程

---
+++ software/lesson1_new_project/p1_hello_world/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,19 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "D:/Espressif/frameworks/esp-idf-v4.4.3/components/bootloader/subproject"
+  "E:/Git/ESP32-S3/software/lesson1_new_project/p1_hello_world/build/bootloader"
+  "E:/Git/ESP32-S3/software/lesson1_new_project/p1_hello_world/build/bootloader-prefix"
+  "E:/Git/ESP32-S3/software/lesson1_new_project/p1_hello_world/build/bootloader-prefix/tmp"
+  "E:/Git/ESP32-S3/software/lesson1_new_project/p1_hello_world/build/bootloader-prefix/src/bootloader-stamp"
+  "E:/Git/ESP32-S3/software/lesson1_new_project/p1_hello_world/build/bootloader-prefix/src"
+  "E:/Git/ESP32-S3/software/lesson1_new_project/p1_hello_world/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "E:/Git/ESP32-S3/software/lesson1_new_project/p1_hello_world/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
